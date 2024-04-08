1f620d32b4702ee9afd3e6cff44491891579efc8
Mark Descalzo <mdescalzo@mac.com>
2024-04-08 14:12:46 -0600

    Initial Commit

---
+++ CRUD Template/CRUD_TemplateApp.swift
@@ -0,0 +1,20 @@
+//
+//  CRUD_TemplateApp.swift
+//  CRUD Template
+//
+//  Created by Mark Descalzo on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_TemplateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
